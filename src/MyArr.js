c9a78be3518dd8ec43e135cd095ec00d67b943e9
unknown <art.pruttov@gmail.com>
2018-12-13 17:59:30 +0200

    fix bugs

---
+++ src/MyArr.js
@@ -0,0 +1,27 @@
+class myArray {
+    constructor() {
+      this.length = arguments.length;
+  
+      for(let i = 0; i < this.length; i++) {
+        this[i] = arguments[i];
+      };
+    }
+    
+    push() {
+        console.log(arguments)
+      for(let i = 0; arguments.length; i++) {         
+        this[this.length] = arguments[i];
+        this.length++;  
+      }        
+    }
+  
+    pop() {
+      let index = this.length-1;
+      let elem = this[index];
+      delete this[index];
+      this.length--;
+      return elem;
+    }
+  };
+
+export default myArray;
