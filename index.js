2989f4cbb8c72ba2593e8322f7ee68052ec426a8
Andr Zakhar <art.pruttov@gmail.com>
2018-12-13 08:55:32 +0200

    add push metod

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 class myArray {
   constructor() {
     this.arguments = arguments;
-    console.log(this.arguments);
+    // console.log(this.arguments);
+    this.length = this.getLength();
     }
 
   getProps(index) {
-    console.log(this.arguments[3]);    
+    // console.log(this.arguments[3]);    
   }; 
   
   getLength() {
@@ -17,7 +18,18 @@ class myArray {
     console.log(`length: ${length}`);
     return length;      
   }
+
+  push(element) {
+    this.arguments[this.length]=element;
+    this.length++;
+  }
+
 };
 
 const arr = new myArray(1, 4, [2, 4], { name: 'Ivan' });
-arr.getLength();
+// arr.getLength();
+console.log(arr.length);
+arr.push('new element');
+console.log(arr.length);
+console.log(arr);
+
